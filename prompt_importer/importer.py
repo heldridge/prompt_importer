c6a6cf2a61fbf152b8b0933a959f1892e695daa2
Harry <eldridgemharry@gmail.com>
2022-05-25 11:18:23 -0400

    Add abstract concept of events

--- prompt_importer/importer.py
+++ prompt_importer/importer.py
@@ -1,16 +1,27 @@
+import abc
 import sqlite3
 
 from beancount.ingest import importer
 
 
-class PromptImporter(importer.ImporterProtocol):
+class Event(abc.ABC):
+    @abc.abstractmethod
+    def get_field(field: str) -> str:
+        pass
+
+
+class PromptImporter(importer.ImporterProtocol, abc.ABC):
     def __init__(self, db_file):
         self.db_file = db_file
 
-    def extract(self, f):
+    @abc.abstractmethod
+    def get_events(self, filename: str) -> list[Event]:
+        pass
+
+    def extract(self, filename: str):
         con = sqlite3.connect(self.db_file)
         cur = con.cursor()
-        columns = "(regex text, recipient text, skip integer)"
+        columns = "(field text, regex text, recipient text, skip integer)"
         print(self.name())
         cur.execute(f"CREATE TABLE if not exists {self.name()} {columns}")
 
